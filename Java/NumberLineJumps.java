92be4507255c535fbcd84aa404ef69bdc8b2ada1
hp0101 <hpphann@gmail.com>
2022-05-18 19:14:06 +0000

    Added NumberLineJumps.java

--- Java/NumberLineJumps.java
+++ Java/NumberLineJumps.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class NumberLineJump {
+class Result {
         /*
      * Complete the 'kangaroo' function below.
      *
@@ -19,12 +19,22 @@ public class NumberLineJump {
      */
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
-        // Write your code here
+
+        // If kangoroo 1's jump is less than kangoroo 2's jump, kangoroo 1 will never catch up 
+        if (v1 - v2 <= 0) return "NO";
+
+        // x1 + y*v1 = x2 + y*v2
+        // x1 - x2 = y*v2 - y*v1
+        // x1 - x2 = y (v2 - v1)
+        // If (x1-x2) % (v2-v1) = 0, then 2 kangaroos meet each other
+        if ((x1-x2) % (v2-v1) == 0) return "YES";
+        
+        return "NO";
     
         }
 }
 
-public class Solution {
+public class NumberLineJumps {
     public static void main(String[] args) throws IOException {
         // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -42,6 +52,7 @@ public class Solution {
         int v2 = Integer.parseInt(firstMultipleInput[3]);
 
         String result = Result.kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
 
         // bufferedWriter.write(result);
         // bufferedWriter.newLine();
